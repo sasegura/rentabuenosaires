5487b0e0b08d252c05bf2c4f8db73ccf31c5e558
sasegura <sasegura.fernandez87@gmail.com>
2021-03-08 18:16:33 +0100

    Borrando

--- src/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Profile = () => {
-    return (
-        <div className="container mt-3">
-            Datos de usuario
-        </div>
-    );
-}
--- src/noHashQueryStringUtils.js
+++
@@ -1,7 +0,0 @@
-import {BasicQueryStringUtils} from '@openid/appauth';
-
-export class NoHashQueryStringUtils extends BasicQueryStringUtils {
-    parse(input, useHash) {
-        return super.parse(input, false /* never use hash */);
-    }
-}
