db5ae6c93758ea959c1d8c7c492fecf0f4b71e8f
sasegura <sasegura.fernandez87@gmail.com>
2021-03-12 13:42:55 +0100

    piso

--- src/pages/Piso.js
+++ src/pages/Piso.js
@@ -27,26 +27,32 @@ const items = [
 function Piso(){
   const [activeIndex, setActiveIndex] = React.useState(0);
   const [animating, setAnimating] = React.useState(false);
+  
   const onExiting = () => {
     setAnimating(true);
   };
+
   const onExited = () => {
     setAnimating(false);
   };
+
   const next = () => {
     if (animating) return;
     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
+
   const previous = () => {
     if (animating) return;
     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
+
   const goToIndex = newIndex => {
     if (animating) return;
     setActiveIndex(newIndex);
   };
+
   return (
     <>
       <Carousel activeIndex={activeIndex} next={next} previous={previous}>
