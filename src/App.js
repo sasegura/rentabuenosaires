a5eaf65ee7e385cb820823af12d6e197c2e3d247
sasegura <sasegura.fernandez87@gmail.com>
2021-04-08 18:26:23 +0200

    fgjh

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import "./App.scss"
 
 function App() {
     return (
